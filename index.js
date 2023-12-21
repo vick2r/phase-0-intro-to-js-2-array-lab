0ffbf16ae88ae2dcb7a161d25b5f1ba230b5eafb
Victor Njoroge <victorknjoroge@gmail.com>
2023-12-21 04:05:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
